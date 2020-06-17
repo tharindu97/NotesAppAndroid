bb384bfc4f3debc270ff3e21037b5a8fcf20a827
tharindu97 <tharindukavishna@gmail.com>
2020-06-17 15:26:07 +0530

    simple_ui_part_complete

---
+++ app/src/main/java/com/example/notes/ui/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.notes.ui
+
+
+import android.os.Bundle
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+
+import com.example.notes.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        button_add.setOnClickListener {
+            val action = HomeFragmentDirections.actionAddNote()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
