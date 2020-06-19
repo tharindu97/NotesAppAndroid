6c139469ac5ba2f64e1895ca5c200d7b7252f340
tharindu97 <tharindukavishna@gmail.com>
2020-06-19 11:15:19 +0530

    have_bugs

--- app/src/main/java/com/example/notes/db/NoteDatabase.kt
+++ app/src/main/java/com/example/notes/db/NoteDatabase.kt
@@ -1,9 +1,14 @@
 package com.example.notes.db
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database (
+    entities =  [Note::class],
+    version = 1
+)
 abstract  class NoteDatabase : RoomDatabase(){
       abstract  fun getNoteDao () : NoteDao
 
